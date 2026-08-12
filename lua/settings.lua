d9c6b583007aed3e4772ee60b187d7cbb63b3cc6
Florian <florian.thamm03+github@proton.me>
2026-08-12 12:35:35 +0200

    added telescope plugin, updated settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,5 +1,5 @@
 -- Basic Options
-vim.cmd("colorscheme kanagawa-wave")
+vim.cmd('colorscheme kanagawa-wave')
 
 
 vim.opt.number = true
@@ -8,5 +8,5 @@ vim.opt.wrap = true
 
 
 -- Keybinds
-vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
-vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>")
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc='Write File' })
+vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', { desc='Quit File' })
