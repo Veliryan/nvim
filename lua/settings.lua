fad0dad11d4b03187cf948022d1ee4f9905a011c
Florian <florian.thamm03+github@proton.me>
2026-08-12 11:55:54 +0200

    initial README, added structure, added lazy package manager, added basic settings

---
+++ lua/settings.lua
@@ -0,0 +1,12 @@
+-- Basic Options
+vim.cmd("colorscheme kanagawa-wave")
+
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = true
+
+
+-- Keybinds
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>")
+vim.keymap.set("n", "<leader>q", "<cmd>quit<cr>")
