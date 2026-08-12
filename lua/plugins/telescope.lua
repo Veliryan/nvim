0b3402d4f7a70a9b2b8a994df2db1cb5e4c375ec
Florian <florian.thamm03+github@proton.me>
2026-08-12 14:19:01 +0200

    added telescope navigation

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,5 +15,13 @@ return {
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+		vim.keymap.set("n", "<leader>fn", function()
+			builtin.grep_string({
+				search = "@nav:",
+				use_regex = false,
+			})
+		end, {
+		desc = "Navigation comments",
+	})
 	end,
 }
