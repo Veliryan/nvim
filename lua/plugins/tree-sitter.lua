bac54b1ee3fa74f5964543898f05e7bf1c42199d
Florian <florian.thamm03+github@proton.me>
2026-08-15 10:34:12 +0200

    updated: tree-sitter (removed function: folding, indentation)

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -10,7 +10,7 @@ return {
 			install_dir = vim.fn.stdpath('data') .. '/site',
 		}
 
-		-- ensure installed 
+		-- ensure installed
 		ts.install {
 			'bash',
 			'c',
@@ -30,20 +30,9 @@ return {
 				-- is parser already installed?
 				if vim.treesitter.language.add(lang) then
 					vim.treesitter.start(args.buf, lang)
-
-					-- Folding
-					vim.wo.foldmethod = 'expr'
-					vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
-					-- Indentation, if Query is found
-					if vim.treesitter.query.get(lang, 'indents') then
-						vim.bo.indentexpr =
-							"v:lua.require'nvim-treesitter'.indentexpr()"
-					end
-
 					return
 				end
-				
+
 				-- parser available but not installed?
 				if vim.tbl_contains(ts.get_available(), lang) then
 					ts.install({ lang }):await(function()
@@ -51,15 +40,6 @@ return {
 							and vim.treesitter.language.add(lang)
 						then
 							vim.treesitter.start(args.buf, lang)
-
-							vim.wo[0][0].foldmethod = 'expr'
-							vim.wo[0][0].foldexpr =
-								'v:lua.vim.treesitter.foldexpr()'
-
-							if vim.treesitter.query.get(lang, 'indents') then
-								vim.bo[0][0].indentexpr =
-									"v:lua.require'nvim-treesitter'.indentexpr()"
-							end
 						end
 					end)
 				end
