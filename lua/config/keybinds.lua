a4889fe774b6a633746708434ec1cd89f16c27f2
Florian <florian.thamm03+github@proton.me>
2026-08-15 10:22:56 +0200

    updated: keybinds (resize splits, quitall)

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,9 +1,10 @@
-	-- Almost all Plugin Specific Keybinds are found inside the <plugin>.lua
+-- Almost all Plugin Specific Keybinds are found inside the <plugin>.lua
 
 -- File Keybinds
 -- save/quit --
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Write' })
 vim.keymap.set('n', '<leader>q', '<cmd>quit<cr>', { desc = 'Close' })
+vim.keymap.set('n', '<leader>x', '<cmd>quitall!<cr>', { desc = 'Quitall' })
 
 
 -- Split Keybinds
@@ -17,6 +18,18 @@ vim.keymap.set('n', '<leader><LEFT>', '<cmd>wincmd h<cr>', { desc = 'Focus Left 
 vim.keymap.set('n', '<leader><RIGHT>', '<cmd>wincmd l<cr>', { desc = 'Focus Right Split' })
 vim.keymap.set('n', '<leader><UP>', '<cmd>wincmd k<cr>', { desc = 'Focus Up Split' })
 vim.keymap.set('n', '<leader><DOWN>', '<cmd>wincmd j<cr>', { desc = 'Focus Down Split' })
+
+-- slow resize --
+vim.keymap.set('n', '<M-Left>', '<cmd>vertical resize +2<cr>', { desc = 'Decrease Split Width' })
+vim.keymap.set('n', '<M-Right>', '<cmd>vertical resize -2<cr>', { desc = 'Increase Split Width' })
+vim.keymap.set('n', '<M-Up>', '<cmd>resize -2<cr>', { desc = 'Increase Split Height' })
+vim.keymap.set('n', '<M-Down>', '<cmd>resize +2<cr>', { desc = 'Decrease Split Height' })
+
+-- fast resize --
+vim.keymap.set('n', '<M-S-Left>', '<cmd>vertical resize +10<cr>', { desc = 'Decrease Split Width Fast' })
+vim.keymap.set('n', '<M-S-Right>', '<cmd>vertical resize -10<cr>', { desc = 'Increase Split Width Fast' })
+vim.keymap.set('n', '<M-S-Up>', '<cmd>resize -10<cr>', { desc = 'Increase Split Height Fast' })
+vim.keymap.set('n', '<M-S-Down>', '<cmd>resize +10<cr>', { desc = 'Decrease Split Height Fast' })
 
 
 -- Clipboard
