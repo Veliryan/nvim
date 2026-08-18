c0357f4453a39126b497d84f18c148352af7d33e
Florian <florian.thamm03+github@proton.me>
2026-08-18 06:59:08 +0200

    Add key mappings for Harpoon file selection
    Added key mappings for selecting files 2 to 6 in Harpoon.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -17,18 +17,28 @@ return {
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = "Harpoon: Open menu" })
 
-    vim.keymap.set("n", "<leader>hw", function()
-      harpoon:list():next()
-    end, { desc = "Harpoon: Next file" })
-
-    vim.keymap.set("n", "<leader>hb", function()
-      harpoon:list():prev()
-    end, { desc = "Harpoon: Previous file" })
-
-    --[[
-		vim.keymap.set("n", "<leader>1", function()
-			harpoon:list():select(1)
-		end, { desc = "Harpoon: File 1" })
-		--]]
+	vim.keymap.set("n", "<leader>1", function()
+		harpoon:list():select(1)
+	end, { desc = "Harpoon: File 1" })
+		
+	vim.keymap.set("n", "<leader>2", function()
+		harpoon:list():select(2)
+	end, { desc = "Harpoon: File 2" })
+	
+	vim.keymap.set("n", "<leader>3", function()
+		harpoon:list():select(3)
+	end, { desc = "Harpoon: File 3" })
+	
+	vim.keymap.set("n", "<leader>4", function()
+		harpoon:list():select(4)
+	end, { desc = "Harpoon: File 4" })
+	
+	vim.keymap.set("n", "<leader>5", function()
+		harpoon:list():select(5)
+	end, { desc = "Harpoon: File 5" })
+	
+	vim.keymap.set("n", "<leader>6", function()
+		harpoon:list():select(6)
+	end, { desc = "Harpoon: File 6" })
   end,
 }
